930ba091992e2adfc29d35dc7acb24555aae1a8e
kosovic-drasko <drasko.kosovic@gmail.com>
2022-03-07 21:46:21 +0100

    init

--- src/main/webapp/app/entities/grafikon/grafikon.module.ts
+++ src/main/webapp/app/entities/grafikon/grafikon.module.ts
@@ -3,9 +3,10 @@ import { SharedModule } from 'app/shared/shared.module';
 import { GrafikonComponent } from './list/grafikon.component';
 import { GrafikonDetailComponent } from './detail/grafikon-detail.component';
 import { GrafikonRoutingModule } from './route/grafikon-routing.module';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [SharedModule, GrafikonRoutingModule],
+  imports: [SharedModule, GrafikonRoutingModule, MatButtonModule],
   declarations: [GrafikonComponent, GrafikonDetailComponent],
 })
 export class GrafikonModule {}
